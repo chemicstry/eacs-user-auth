a22796d8a3b62698cd3627d8e6e6ce31509c5d3f
chemicstry <chemicstry@gmail.com>
2018-05-10 01:25:52 +0300

    Implement simple LDAP authentication

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,130 @@
+import commandLineArgs = require('command-line-args');
+import commandLineUsage = require('command-line-usage');
+import { EACSSocket, EACSToken } from 'eacs-socket';
 import * as WebSocket from 'ws';
+import { readFileSync } from 'fs';
+import { IncomingMessage } from 'http';
 import { Log } from './Log';
 import { WSTransport, RPCNode } from 'modular-json-rpc';
+import { RPCMethodError } from 'modular-json-rpc/dist/Defines';
+import optionDefinitions from './options';
+import * as ldap from 'ldapjs';
 
-const wss = new WebSocket.Server({ port: 3001 });
+// Options
+const options = commandLineArgs(optionDefinitions);
 
-wss.on('connection', (ws: WebSocket) => {
-    Log.info("index: New websocket connection");
+// Print usage
+if (options.help)
+{
+    const sections = [
+        {
+            header: 'eacs-user-auth',
+            content: 'Extensible Access Control System. User Authentication Module.'
+        },
+        {
+            header: 'Options',
+            optionList: optionDefinitions
+        }
+    ];
+
+    console.log(commandLineUsage(sections));
+    process.exit();
+}
+
+// Load JWT public key
+const jwtPublicKey = readFileSync(options.jwtPublicKey, "utf8");
+
+// Setup EACSSocket (websockets with JWT auth)
+const socket = new EACSSocket({
+    host: options.host,
+    port: options.port,
+    jwtPubKey: jwtPublicKey
+});
+
+enum RPCErrors
+{
+    UNSUPPORTED_TAG_TYPE                = 1,
+    AUTHENTICATE_FAILED                 = 2,
+    INITIALIZE_KEY_FAILED               = 3,
+    ACCESS_DENIED                       = 4,
+}
+
+// Initialize LDAP client
+var ldapClient = ldap.createClient({
+    url: options.ldapURL
+});
+
+// Authenticate LDAP
+ldapClient.bind(options.ldapUser, options.ldapPass, (err) => {
+    if (err)
+        throw new Error(`LDAP bind failed: ${err}`);
+    else
+        Log.info('index: LDAP bind successful.');
+});
+
+function asyncLDAPSearch(base: string, filter: string): Promise<any[]>
+{
+    return new Promise((resolve, reject) => {
+        var opts = {
+            scope: 'sub',
+            filter
+        };
+
+        var results: any[] = [];
+        ldapClient.search(base, opts, function(err, res) {
+            if (err)
+            {
+                Log.error(`index: LDAP search error: ${err}`);
+                return;
+            }
+        
+            res.on('searchEntry', function(entry) {
+                results.push(entry.object);
+            });
+            res.on('error', function(err) {
+                reject(err);
+            });
+            res.on('end', function(result) {
+                resolve(results);
+            });
+        });
+    })
+}
+
+async function findUserByUID(uid: string)
+{
+    if (!uid.match(/^[a-z0-9]+$/i))
+        throw new Error('Invalid UID format');
+    
+    var users = await asyncLDAPSearch(
+        'dc=example,dc=com',
+        `(&(associatedDomain=${uid}))`
+    );
+
+    Log.debug(`findUserByUID: users: ${JSON.stringify(users)}`);
+
+    if (!users.length)
+        throw new Error('User not found');
+
+    return users[0];
+}
+
+async function userOfGroupWithPermission(uid: string, perm: string)
+{
+    var groups = await asyncLDAPSearch(
+        'dc=example,dc=com',
+        `(&(memberUid=${uid})(bootFile=${perm}))`
+    );
+
+    Log.debug(`userOfGroupWithPermission: groups: ${JSON.stringify(groups)}`);
+
+    return groups.length > 0;
+}
+
+socket.on('connection', (ws: WebSocket, req: IncomingMessage) => {
+    let token = <EACSToken>(<any>req).token;
+
+    Log.info(`index: New connection from ${req.connection.remoteAddress}. Identifier: ${token.identifier}`);
 
     // Create RPC transport over websocket
     let transport = new WSTransport(ws);
@@ -22,11 +141,22 @@ wss.on('connection', (ws: WebSocket) => {
     });
 
     // object - permission (i.e. main door, lights, etc)
-    node.bind("auth_uid", (object: string, uid: string) => {
-        if (uid === "046981BA703A80")
-            return true;
-        else
+    node.bind("auth_uid", async (object: string, uid: string) => {
+        // Check password for this method
+        if (!token.hasPermission("eacs-user-auth:auth_uid"))
+            throw new RPCMethodError(RPCErrors.ACCESS_DENIED, 'No permission to call auth_uid');
+
+        try {
+            let user = await findUserByUID(uid);
+            Log.debug(`auth_uid: user: ${JSON.stringify(user)}`);
+
+            let auth = await userOfGroupWithPermission(user.uid, token.identifier);
+
+            return auth;
+        } catch (err) {
+            Log.debug(`auth_uid: find user error: ${err}`);
             return false;
+        }
     });
 });
 
